07b7483c84c64d598f11759b0da65b38be3f2518
Nathaniel Bogaty <nathanielbogaty@gmail.com>
2024-10-30 08:48:26 -0400

    adding SQLAlchemy (SQLite version) to flask app

--- python_app/app.py
+++ python_app/app.py
@@ -1,10 +1,32 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+
+# Configure SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Define a basic model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+# Create database tables
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @app.route('/')
 def home():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
